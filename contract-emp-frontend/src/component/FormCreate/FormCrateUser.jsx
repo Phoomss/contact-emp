2e61d33a0596af6c704dda261513272bff0b55fd
Tudzy <narongsak5az@gmail.com>
2023-06-07 10:08:34 +0700

    user

--- contract-emp-frontend/src/component/FormCreate/FormCrateUser.jsx
+++ contract-emp-frontend/src/component/FormCreate/FormCrateUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import {
   Box,
   Button,
@@ -12,52 +12,76 @@ import {
 import UserService from "services/UserService";
 import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
+import CompanyService from "services/CompanyService";
 
 const FormCreateUser = () => {
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
   const [email, setEmail] = useState("");
-  const [telephone, setTelePhone] = useState("");
-  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
   const [companyId, setCompanyId] = useState("");
+  const [createUser, setCreateUser] = useState({})
+  const [userId, setUserId] = useState(0)
+  const [companies, setCompanies] = useState([]);
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
-  const [createuser, setCreateUser] = useState({});
-  const [userId, setUserId] = useState(0);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  // const [telephone,setTelePhone] = useState("")
+  // const [username,setUserName] = useState("")
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await UserService.postRegister({
-        name,
-        surname,
-        telephone,
-        username,
-        email,
-        password,
-        role,
-        company_id: companyId,
+        name: name,
+        surname: surname,
+        // telephone: telephone,
+        // username: username,
+        email: email,
+        password: password,
+        role: role,
+        company_Id: companyId, // แนบ company_Id ในข้อมูลที่ส่ง
       });
-      if (response.status === 200) {
-        swal("เพิ่มลูกจ้างสำเร็จ!", "success");
+      if (response && response.data) { // เพิ่มเงื่อนไขการตรวจสอบ response.data
+        swal("เพิ่มผู้ใช้งานสำเร็จ", "success");
         console.log(response.data);
         setCreateUser(response.data.data);
-        setUserId(response.data.data.id);
+        setUserId(response.data.data);
+
+        // ดึงขื่อบริษัทมาแสดง
+        const responseCompanies = await CompanyService.getCompanies()
+        if (responseCompanies && responseCompanies.data) {
+          setCompanies(responseCompanies.data)
+        }
         navigate("/userall");
       }else{
-        setMessage(response.data.error || "Something went wrong!");
+        setError("Can not created")
       }
     } catch (error) {
       console.error("Error:", error.response);
-      setError(error.response.data.message);
+      setError(error.response?.data?.message); // ปรับเปลี่ยนเพื่อให้ดึงข้อความผิดพลาดจาก response.data.message ถ้ามี
     }
   };
 
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const response = await CompanyService.getCompanies();
+        if (response && response.data) {
+          setCompanies(response.data);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+
   const handleCancelClick = () => {
-    navigate("/userall");
+    navigate("/userall")
   };
 
   return (
@@ -69,6 +93,7 @@ const FormCreateUser = () => {
               <InputLabel>Name:</InputLabel>
               <TextField
                 value={name}
+                type="text"
                 onChange={(e) => setName(e.target.value)}
                 fullWidth
               />
@@ -77,26 +102,29 @@ const FormCreateUser = () => {
               <InputLabel>Surname:</InputLabel>
               <TextField
                 value={surname}
+                type="text"
                 onChange={(e) => setSurname(e.target.value)}
                 fullWidth
               />
             </Grid>
-            <Grid item xs={6}>
-              <InputLabel>เบอร์โทรศัพท์:</InputLabel>
+            {/* <Grid item xs={6}>
+              <InputLabel>telephone:</InputLabel>
               <TextField
                 value={telephone}
+                type="text"
                 onChange={(e) => setTelePhone(e.target.value)}
                 fullWidth
               />
             </Grid>
             <Grid item xs={6}>
-              <InputLabel>ยูสเซอร์เนม:</InputLabel>
+              <InputLabel>username:</InputLabel>
               <TextField
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                onChange={(e) => setUserName(e.target.value)}
                 fullWidth
               />
-            </Grid>
+            </Grid> */}
             <Grid item xs={6}>
               <InputLabel>Email:</InputLabel>
               <TextField
@@ -123,7 +151,6 @@ const FormCreateUser = () => {
                 fullWidth
               >
                 <MenuItem value="">Select Role</MenuItem>
-                <MenuItem value="admin">Admin</MenuItem>
                 <MenuItem value="card">Card</MenuItem>
                 <MenuItem value="company">Company</MenuItem>
               </Select>
@@ -131,11 +158,12 @@ const FormCreateUser = () => {
             {role === "card" && (
               <Grid item xs={6}>
                 <InputLabel>Company ID:</InputLabel>
-                <TextField
-                  value={companyId}
-                  onChange={(e) => setCompanyId(e.target.value)}
-                  fullWidth
-                />
+                <Select value={companyId} onChange={(e) => setCompanyId(e.target.value)} fullWidth>
+                <MenuItem value="">Select Company</MenuItem>
+                {companies.map((company)=>(
+                  <MenuItem key={company.id} value={company.id}>{company.name}</MenuItem>
+                ))}
+                </Select>
               </Grid>
             )}
             <Grid item xs={12}>
@@ -158,10 +186,7 @@ const FormCreateUser = () => {
           </Typography>
         )}
         {error && (
-          <Typography
-            variant="body1"
-            sx={{ marginTop: "1rem", color: "error" }}
-          >
+          <Typography variant="body1" sx={{ marginTop: "1rem", color: "error" }}>
             {error}
           </Typography>
         )}
