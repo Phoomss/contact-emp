41d2a79adbae369b73a37934b30ecd9abbab7f70
Tudzy <narongsak5az@gmail.com>
2023-06-24 18:50:10 +0700

    footer

--- contract-emp-frontend/src/component/Layouts/Footer.jsx
+++ contract-emp-frontend/src/component/Layouts/Footer.jsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 import { Box } from "@mui/material";
 
 const Footer = () => {
@@ -12,16 +9,15 @@ const Footer = () => {
       component="footer"
       sx={{
         backgroundColor: '#FFCC00',
-        p: 6,
-       marginTop: 9
+        p: 1,
       }}
     >
       <Container maxWidth="lg">
         <Box>
-          <Typography align="center" style={{color: '#154295'}}>
+          <Typography align="lift" style={{color: '#154295'}}>
           เจ้าของระบบ : แผนกอุปกรณ์และเทคโนโลยีรักษาความปลอดภัย (หอทป-ห.) กองปฏิบัติการรักษาความปลอดภัย (กปป-ห.) ฝ่ายความปลอดภัย (อปภ.) โทร. 64281
             <br />
-            พัฒนาโดย กองบริหารระบบงานดิจิทัล (กบด-ห.) ฝ่ายจัดการและพัฒนาระบบดิจิทัล (อจท.) เบอร์ xxx-xxx-xxxx
+            พัฒนาโดย กองบริหารระบบงานดิจิทัล (กบด-ห.) ฝ่ายจัดการและพัฒนาระบบดิจิทัล (อจท.) เบอร์ 6642
           </Typography>
         </Box>
       </Container>
