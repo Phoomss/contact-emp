ec3ed8c14c27e49f33d17b11a721776f5a57caa1
Tudzy <narongsak5az@gmail.com>
2023-09-11 10:38:05 +0700

    change user route in server.js

--- contract-emp-backend/server.js
+++ contract-emp-backend/server.js
@@ -29,7 +29,7 @@ app.use('/company',(companyRoute))
 app.use('/contract',(contractRoute))
 app.use('/employee',(employeeRoute))
 app.use('/emp',hrRoute)
-app.use('/user',(userRoute))
+app.use('user',(userRoute))
 app.use('/dept',deptRoute)
 
 app.get("/", (req, res)=>{
